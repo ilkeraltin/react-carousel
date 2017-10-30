ac36be3f75f64dc40081f9c51927b1468a10191e
ilker ALTIN <ilkerobi@gmail.com>
2017-10-30 22:24:35 +0300

    change build options, responsive css edits

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import App from './components/App';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
